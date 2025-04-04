0e5bad47cf78c192b0ee22abaf9d653166281fc8
lrseunc <159162132+lrseunc@users.noreply.github.com>
2025-04-04 15:48:17 +0800

    Update groupview.vue

--- groupview.vue
+++ groupview.vue
@@ -14,7 +14,7 @@
           <i class="fas fa-user-circle" style="font-size: 30px; cursor: pointer;"></i>
         </router-link>
       </ul>
-    <div class="icon">
+      <div class="icon">
         <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{'icon-active': mobileNav }"></i>
       </div>
       <transition name="mobile-nav">
@@ -34,80 +34,93 @@
 
   <div class="con">
     <div class="con-container">
-    <div class="nav-con">
-      <router-link to="/view" class="link">
-            <button :class="{ active: $route.path === '/view' }">
-                Personal Expenses
-            </button>
+      <div class="nav-con">
+        <router-link to="/view" class="link">
+          <button :class="{ active: $route.path === '/view' }">
+            Personal Expenses
+          </button>
         </router-link>
         <button @click="currentView = 'groupview'" :class="{ active: $route.path === '/groupview' }">
-            Group Expenses
+          Group Expenses
         </button>
-    </div>
+      </div>
 
-    <div v-if="currentView === 'view'" class="budget-section">
-      <div class="content-wrapper">
-        <!-- Filter Section -->
-        <div class="filter-buttons">
-          <form @submit.prevent>
-            <button @click="filterExpenses('Food')" :class="{ active: filterCategory === 'Food' }">Food</button>
-            <button @click="filterExpenses('Bill')" :class="{ active: filterCategory === 'Bill' }">Bill</button>
-            <button @click="filterExpenses('Transportation')" :class="{ active: filterCategory === 'Transportation' }">Transportation</button>
-            <button @click="filterExpenses('Other')" :class="{ active: filterCategory === 'Other' }">Other</button>
-            <button @click="filterExpenses('all')" :class="{ active: filterCategory === 'all' }">View All</button>
-            
-            <!-- Member Search -->
-            <input type="text" v-model="searchMember" placeholder="Search Member" />
-            <input type="date" v-model="filterDate" />
-            <button @click="filterExpensesByDate" title="Search">
+      <div v-if="currentView === 'view'" class="budget-section">
+        <div class="content-wrapper">
+          <!-- Filter Section -->
+          <div class="filter-buttons">
+            <form @submit.prevent>
+              <button @click="filterExpenses('Food')" :class="{ active: filterCategory === 'Food' }">Food</button>
+              <button @click="filterExpenses('Bill')" :class="{ active: filterCategory === 'Bill' }">Bill</button>
+              <button @click="filterExpenses('Transportation')" :class="{ active: filterCategory === 'Transportation' }">Transportation</button>
+              <button @click="filterExpenses('Other')" :class="{ active: filterCategory === 'Other' }">Other</button>
+              <button @click="filterExpenses('all')" :class="{ active: filterCategory === 'all' }">View All</button>
+              
+              <!-- Member Search -->
+              <input type="text" v-model="searchMember" placeholder="Search Member" />
+              <input type="date" v-model="filterDate" />
+              <button @click="filterExpensesByDate" title="Search">
                 <i class="fa fa-search"></i>
-            </button>
-          </form>
-        </div>
+              </button>
+            </form>
+          </div>
 
-        <!-- Expense Table -->
-        <div class="expense-table">
-          <table>
-            <thead>
-              <tr>
-                <th>Member</th>
-                <th>Category</th>
-                <th>Name</th>
-                <th>Amount</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(expense, index) in filteredExpenses" :key="expense.id" :class="{'alternate-row': index % 2 !== 0}">
-                <td>{{ expense.member }}</td>
-                <td>{{ expense.category }}</td>
-                <td>{{ expense.name }}</td>
-                <td>{{ formatCurrency(expense.amount) }}</td>
-                <td>{{ expense.date }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+          <!-- Expense Table -->
+          <div class="expense-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>Member</th>
+                  <th>Category</th>
+                  <th>Name</th>
+                  <th>Amount</th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(expense, index) in filteredExpenses" :key="expense.id" :class="{'alternate-row': index % 2 !== 0}">
+                  <td>{{ expense.member }}</td>
+                  <td>{{ expense.category }}</td>
+                  <td>{{ expense.name }}</td>
+                  <td>{{ formatCurrency(expense.amount) }}</td>
+                  <td>{{ expense.date }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-        <!-- Display Total Amount -->
-        <div class="total-amount">
-          <p>Total: {{ formatCurrency(totalAmount) }}</p>
-        </div>
+          <!-- Display Total Amount -->
+          <div class="total-amount">
+            <p>Total: {{ formatCurrency(totalAmount) }}</p>
+          </div>
 
-        <div>
-          <pie-chart :data="chartData" 
-          :options="chartOptions" 
-          style="height: 400px; width: auto; justify-self: center;"/>
+          <!-- Pie Chart -->
+          <div>
+            <pie-chart :data="chartData" :options="chartOptions" style="height: 400px; width: auto; justify-self: center;"/>
+          </div>
+
+                   <!-- Year and Month Picker for PDF generation -->
+          <select v-model="selectedYear">
+            <option v-for="year in availableYears" :key="year" :value="year">{{ year }}</option>
+          </select>
+
+          <select v-model="selectedMonth">
+            <option v-for="month in availableMonths" :key="month.value" :value="month.value">{{ month.label }}</option>
+          </select>
+
+          <button class="download-button" @click="generatePDF">Download Report</button>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
+
+
 
 <script>
 import { Pie } from 'vue-chartjs';
 import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale } from 'chart.js';
+import jsPDF from 'jspdf';  
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, LinearScale);
 
@@ -115,7 +128,7 @@ export default {
   name: "Navigation",
   components: {
     PieChart: Pie
-  }, 
+  },
   data() {
     return {
       scrolledNav: null,
@@ -140,6 +153,8 @@ export default {
       ],
       filterCategory: 'all', // Default filter is 'all'
       filterDate: '', // Default date filter is empty
+      selectedYear: '2025', // Default year selected for PDF
+      selectedMonth: '03', // Default month selected for PDF
       chartData: {
         labels: ['Food', 'Bill', 'Transportation', 'Other'], // Categories for the pie chart
         datasets: [{
@@ -168,6 +183,30 @@ export default {
     };
   },
   computed: {
+    availableYears() {
+      const years = new Set(); // To get unique years
+      this.expenses.forEach(expense => {
+        const year = expense.date.split('-')[0];
+        years.add(year);
+      });
+      return Array.from(years).sort(); // Convert to array and sort
+    },
+    availableMonths() {
+      return [
+        { value: '01', label: 'January' },
+        { value: '02', label: 'February' },
+        { value: '03', label: 'March' },
+        { value: '04', label: 'April' },
+        { value: '05', label: 'May' },
+        { value: '06', label: 'June' },
+        { value: '07', label: 'July' },
+        { value: '08', label: 'August' },
+        { value: '09', label: 'September' },
+        { value: '10', label: 'October' },
+        { value: '11', label: 'November' },
+        { value: '12', label: 'December' },
+      ];
+    },
     filteredExpenses() {
       return this.expenses.filter(expense => {
         let categoryMatch = this.filterCategory === 'all' || expense.category === this.filterCategory;
@@ -205,34 +244,76 @@ export default {
     },
     filterExpenses(category) {
       this.filterCategory = category;
+      this.updateChartData(); 
     },
     filterExpensesByDate() {
       if (this.filterDate) {
-        this.filterCategory = 'all';
+        this.filterCategory = 'all'; // Reset category filter when searching by date
+        const yearMonth = this.filterDate.slice(0, 7); // Extract the year and month (e.g., "2025-03")
+        this.filteredExpenses = this.expenses.filter(expense => expense.date.slice(0, 7) === yearMonth); // Filter by year-month
       }
     },
     formatCurrency(value) {
-      return value ? `₱${parseFloat(value).toFixed(2)}` : '₱0.00';
+      if (value == null || isNaN(value)) return '₱0.00'; // Ensure valid numbers
+      return '₱' + parseFloat(value).toFixed(2); // Format to 2 decimal places with currency symbol
     },
     updateChartData() {
-      // Update the data for the pie chart based on filtered expenses
-      const categoryCounts = {
-        Food: 0,
-        Bill: 0,
-        Transportation: 0,
-        Other: 0,
+      let categoryCounts = {
+        'Food': 0,
+        'Bill': 0,
+        'Transportation': 0,
+        'Other': 0
       };
       this.filteredExpenses.forEach(expense => {
-        categoryCounts[expense.category] = categoryCounts[expense.category] + expense.amount || expense.amount;
+        categoryCounts[expense.category] += expense.amount;
       });
+
       this.chartData.datasets[0].data = [
-        categoryCounts.Food,
-        categoryCounts.Bill,
-        categoryCounts.Transportation,
-        categoryCounts.Other,
+        categoryCounts['Food'],
+        categoryCounts['Bill'],
+        categoryCounts['Transportation'],
+        categoryCounts['Other']
       ];
-    }
-  },
+    },
+    generatePDF() {
+      const doc = new jsPDF();
+
+      // Add title at the top of the PDF
+      doc.setFontSize(18);
+      doc.text('Expense Report', 20, 20);
+
+      // Add the total amount to the PDF (formatted with currency)
+      doc.setFontSize(12);
+      doc.text(`Total Expenses: ${this.formatCurrency(this.totalAmount)}`, 20, 30);
+
+      // Initialize the Y offset for rows
+      let yOffset = 40;
+
+      // Filter expenses based on the selected month and year for the PDF
+      const filteredForPDF = this.filteredExpenses.filter(expense => {
+        return expense.date.startsWith(`${this.selectedYear}-${this.selectedMonth}`);
+      });
+
+      // Add each filtered expense as a row in the PDF
+      filteredForPDF.forEach((expense) => {
+        doc.text(`${expense.member} - ${expense.category} - ${expense.name} - ${expense.date}: ${this.formatCurrency(expense.amount)}`, 20, yOffset);
+        yOffset += 10; // Increment the Y offset for the next row
+      });
+
+      // Wait for the chart to be fully rendered
+      this.$nextTick(() => {
+        const chartCanvas = document.querySelector('canvas'); // Assuming the pie chart is in a <canvas> element
+        if (chartCanvas) {
+          const chartImage = chartCanvas.toDataURL('image/png'); // Convert the canvas to a PNG image
+          // Add the pie chart image below the table rows
+          doc.addImage(chartImage, 'PNG', 20, yOffset, 160, 100); // Adjust width and height of the chart image as needed
+          doc.save('expense-report.pdf'); // Save the PDF as a file
+        } else {
+          console.error("Chart not found");
+        }
+      });
+    },
+  }
 };
 </script>
 
@@ -460,5 +541,19 @@ button:hover {
 .total-amount {
   margin-top: 20px;
   font-weight: bold;
+}
+
+.download-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: #2a4935;
+  color: white;
+  border: none;
+  cursor: pointer;
+  margin-top: 20px;
+}
+
+.download-button:hover {
+  background-color: #1e3731;
 }
 </style>
